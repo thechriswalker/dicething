2b81cd9a7fb9b91658ff716012e4985a0f3ca4e8
Chris Walker <chris@thechriswalker.net>
2025-05-14 20:43:23 +0100

    fix: re-enable dark/light mode

--- src/routes/splash/+layout.svelte
+++ src/routes/splash/+layout.svelte
@@ -3,9 +3,9 @@
 	import LightMode from '$lib/components/light_switch/LightMode.svelte';
 
 	let { children } = $props();
-	// <LightMode />
 </script>
 
+<LightMode />
 <div class="relative flex h-screen w-screen flex-col overflow-hidden">
 	{@render children()}
 </div>
